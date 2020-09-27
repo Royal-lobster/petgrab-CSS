d2d5d2b2123f9b7212309beb681e9cfc5ce5b103
Royal-lobster <srujangs8@gmail.com>
2020-09-27 10:03:53 +0530

    given hamburger animation

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-var hamburgerIcon = document.getElementById("hamburgerIcon");
+var hamburgerIcon = document.getElementById("hamburger-6");
 var navUl = document.getElementById("navUL");
 
 let state = "close";
 hamburgerIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("is-active");
+
   console.log("clicked");
   if (state == "close") {
     navUl.style.transform = "translate(0%)";
+    hamburgerIcon.style.position = "fixed";
+    hamburgerIcon.style.right = "40px";
     state = "open";
   } else if (state == "open") {
     navUl.style.transform = "translate(100%)";
+    hamburgerIcon.style.position = "unset";
     state = "close";
   }
   console.log(state);
